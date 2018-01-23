2661ce511ccd425a370d51172fe8f09d5574befb
Greg.Wallerus <gwallerus@gmail.com>
2018-01-23 11:48:09 -0600

    Java Practice

--- Assignment11.java
+++ Assignment11.java
@@ -5,10 +5,10 @@ public class Assignment11 {
 		
 	int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		
-		//setup my outer loop
+		
 		for (int i = 0; i < array1.length; i++){
 			
-			//start inner loop
+			
 			for (int j = 1; j <= 10; j++){
 				int result = array1[i] * j;
 				System.out.println("Result: " + result);
